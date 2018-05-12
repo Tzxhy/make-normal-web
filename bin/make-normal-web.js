4ea1b8202a6bf615aeca59d90b2dcfb1c4aa3eab
tzxhy <tanzhixuan2018@163.com>
2018-05-12 16:46:03 +0800

    fix

--- bin/make-normal-web.js
+++ bin/make-normal-web.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-var buildProject = require('../src/index.js');
+
 if (!process.argv[2]) {
     console.log('使用make-web-dir <your-app-name>来创建静态web项目目录。');
 } else {
+    var buildProject = require('../src/index.js');
     buildProject();
 }
